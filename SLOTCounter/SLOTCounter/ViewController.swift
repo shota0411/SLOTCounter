fb97f6194a2b9095aa579738b9fc88c42da094ec
松田 翔大 <thank2626@gmail.com>
2018-05-08 15:51:42 +0900

    プロジェクト作成

---
+++ SLOTCounter/SLOTCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SLOTCounter
+//
+//  Created by 松田翔大 on 2018/05/05.
+//  Copyright © 2018年 松田翔大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+

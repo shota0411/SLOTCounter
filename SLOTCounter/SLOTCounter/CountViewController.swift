6429c74e945ac3da547d46ac1365f3eee5da3d89
松田 翔大 <thank2626@gmail.com>
2018-05-08 17:58:29 +0900

    マイジャグ３用の仮レイアウト実装

---
+++ SLOTCounter/SLOTCounter/CountViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CountViewController.swift
+//  SLOTCounter
+//
+//  Created by 松田翔大 on 2018/05/08.
+//  Copyright © 2018年 松田翔大. All rights reserved.
+//
+
+import UIKit
+
+class CountViewController: UIViewController {
+
+    @IBOutlet weak var startRotation: UITextField!
+    @IBOutlet weak var endRotation: UITextField!
+    @IBOutlet weak var bigCount: UILabel!
+    @IBOutlet weak var regCount: UILabel!
+    @IBOutlet weak var aloneBigCount: UILabel!
+    @IBOutlet weak var aloneRegCount: UILabel!
+    @IBOutlet weak var overlapBigCount: UILabel!
+    @IBOutlet weak var overlapRegCount: UILabel!
+    @IBOutlet weak var GrapeCount: UILabel!
+    @IBOutlet weak var cherryCount: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
--- SLOTCounter/SLOTCounter/ViewController.swift
+++ SLOTCounter/SLOTCounter/ViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.delegate = self
     }
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
 }
 
